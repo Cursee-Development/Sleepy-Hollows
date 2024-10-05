66733d8a234aac778793c75907fc35e2732ea2f9
jason13official <jason13official@gmail.com>
2024-10-04 20:12:13 -0400

    feat: init 1.20.1 architectury template

---
+++ common/src/main/java/net/satisfy/sleepy_hollows/ExampleMod.java
@@ -0,0 +1,9 @@
+package net.satisfy.sleepy_hollows;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "sleepy_hollows";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
